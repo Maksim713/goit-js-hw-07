dcaa51a1ab49738f10591f3077035d637c3b15f5
Maxim Sytnik <118841889+Maksim713@users.noreply.github.com>
2023-05-19 18:36:29 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryUl = document.querySelector(".gallery");
+
+const arrayImg = [];
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a href="${original}" class="gallery__link"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></li>`
+  )
+  .join("");
+
+arrayImg.push(markup);
+
+console.log(markup);
+
+galleryUl.insertAdjacentHTML("beforeend", arrayImg);
+
+galleryUl.addEventListener("click", openImageClick);
+
+function openImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<div class="modal">
+        <img src="${event.target.dataset.source}" width="800" height="600">
+    </div>`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onEscape);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onEscape);
+      },
+    }
+  );
+
+  instance.show();
+
+  const modalRef = document.querySelector(".modal");
+  modalRef.addEventListener("click", () => {
+    instance.close();
+  });
+
+  function onEscape(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryUl = document.querySelector(".gallery");
+
+const arrayImg = [];
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a href="${original}" class="gallery__link"><img class="gallery__image" src="${preview}" alt="${description}"></a></li>`
+  )
+  .join("");
+
+arrayImg.push(markup);
+
+console.log(markup);
+
+galleryUl.insertAdjacentHTML("beforeend", arrayImg);
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
